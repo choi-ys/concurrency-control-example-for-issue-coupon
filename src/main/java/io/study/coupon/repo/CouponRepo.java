1eeef546449a5d3b7ec591e21a091b96e937c89b
choi-ys <project.log.062@gmail.com>
2022-10-24 19:41:16 +0900

    feat(#7): JPA Pessimistic Lock을 이용한 쿠폰 조회 구현 및 TC 작성
    - JPA의 Pessimistic Lock(비관락)을 통해 row에 대해 다른 트랜잭션의 읽기/쓰기를 제한하는 Exclusive Lock(배타락)을 걸어 Application에서 동시성을 제어하기 위한 조회 쿼리 작성
      - 출력된 수행 쿼리를 통해 베타락 쿼리 여부를 검증하기 위한 ConsoleCaptor 의존성 추가
      - ConsoleCaptor 라이브러리를 이용하여 Console에 출력된 수행 쿼리가 `for update`이 적용된 배타락 쿼리 인지 여부 검증
      - AutoCloseable의 구현체인 ConsoleCaptor의 자원 정리를 위한 try ~ resource 구문 적용

--- src/main/java/io/study/coupon/repo/CouponRepo.java
+++ src/main/java/io/study/coupon/repo/CouponRepo.java
@@ -1,7 +1,15 @@
 package io.study.coupon.repo;
 
 import io.study.coupon.entity.Coupon;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CouponRepo extends JpaRepository<Coupon, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select coupon from Coupon as coupon where coupon.id = :id")
+    Optional<Coupon> findByIdWithPessimisticLock(@Param("id") Long id);
 }
---
+++ src/test/java/io/study/coupon/repo/CouponRepoPessimisticLockTest.java
@@ -0,0 +1,50 @@
+package io.study.coupon.repo;
+
+import static io.study.utils.generator.fixture.CouponFixtureGenerator.수량이_100개인_쿠폰_생성;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.study.config.AbstractDataJpaTestBase;
+import io.study.coupon.entity.Coupon;
+import java.util.Optional;
+import nl.altindag.console.ConsoleCaptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest(showSql = true)
+@DisplayName("Repo:Coupon:Pessimistic Lock")
+public class CouponRepoPessimisticLockTest extends AbstractDataJpaTestBase {
+    private static final String PESSIMISTIC_LOCK_QUERY_KEYWORD = "for update";
+    private final CouponRepo couponRepo;
+
+    public CouponRepoPessimisticLockTest(CouponRepo couponRepo) {
+        this.couponRepo = couponRepo;
+    }
+
+    @Test
+    @DisplayName("JPA가 제공하는 PessimisticLock의 수행 쿼리 검증")
+    public void concurrentControlByJpaPessimisticLock() {
+        // Given
+        final Coupon 저장된_수량이_100개인_쿠폰 = executeWithPersistContextClear(
+            () -> couponRepo.save(수량이_100개인_쿠폰_생성())
+        );
+
+        // When
+        Optional<String> 베타락_키워드가_포함된_실행_쿼리_조각 = executeWithAroundConsoleCaptor(
+            () -> couponRepo.findByIdWithPessimisticLock(저장된_수량이_100개인_쿠폰.getId()).orElseThrow()
+        );
+
+        // Then
+        assertThat(베타락_키워드가_포함된_실행_쿼리_조각.isPresent()).isTrue();
+    }
+
+    private Optional<String> executeWithAroundConsoleCaptor(Runnable runnable) {
+        try (ConsoleCaptor consoleCaptor = new ConsoleCaptor()) {
+            runnable.run();
+            return consoleCaptor.getStandardOutput()
+                .stream()
+                .filter(it -> it.contains(PESSIMISTIC_LOCK_QUERY_KEYWORD))
+                .findFirst();
+        }
+    }
+}
